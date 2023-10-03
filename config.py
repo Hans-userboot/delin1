823a4f666a3b1bd8e800475aeeab205645f94ea6
Hans-userboot <100776056+Hans-userboot@users.noreply.github.com>
2023-10-04 00:31:56 +0700

    Update config.py

--- config.py
+++ config.py
@@ -10,16 +10,16 @@ load_dotenv("config.env")
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
-API_ID = int(os.environ.get("API_ID", "2040"))
-API_HASH = os.environ.get("API_HASH", "b18441a1ff607e10a989891a5462e627")
+API_ID = int(os.environ.get("API_ID", "26155203"))
+API_HASH = os.environ.get("API_HASH", "d68547f2c4cc5169b19cb8528783f97a")
 
-CHANNEL_DB = int(os.environ.get("CHANNEL_DB", ""))
-DATABASE_URL = os.environ.get("DATABASE_URL", "")
+CHANNEL_DB = int(os.environ.get("CHANNEL_DB", "-1001724419080"))
+DATABASE_URL = os.environ.get("DATABASE_URL", "postgres://klzdteve:USezQFwkPLpa1F-qH0IiW1M2pUxNhyd8@arjuna.db.elephantsql.com/klzdteve")
 
 RESTRICT = strtobool(os.environ.get("RESTRICT", "True"))
 
-FORCE_SUB_1 = int(os.environ.get("FORCE_SUB_1", "0"))
-FORCE_SUB_2 = int(os.environ.get("FORCE_SUB_2", "0"))
+FORCE_SUB_1 = int(os.environ.get("FORCE_SUB_1", "-1001914420895"))
+FORCE_SUB_2 = int(os.environ.get("FORCE_SUB_2", "-1001749525741"))
 FORCE_SUB_3 = int(os.environ.get("FORCE_SUB_3", "0"))
 FORCE_SUB_4 = int(os.environ.get("FORCE_SUB_4", "0"))
 
@@ -41,7 +41,7 @@ FORCE_MESSAGE = os.environ.get(
 )
 
 try:
-    ADMINS = [int(x) for x in (os.environ.get("ADMINS", "").split())]
+    ADMINS = [int(x) for x in (os.environ.get("ADMINS", "6141496735 5178772086 1724560961").split())]
 except ValueError:
     raise Exception("Daftar Admin Anda tidak berisi User ID Telegram yang valid.")
     
